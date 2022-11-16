75eed1b6c2902684e5fbd116b1573d75e673d888
DawidWesierski4 <dawidwesierski4@gmail.com>
2022-11-16 22:31:23 +0100

    removing-the-complex-fun

--- Liczba.h
+++ Liczba.h
@@ -34,23 +34,4 @@ public:
     }
 };
 
-class Complex : public Wartosc_Liczbowa
-{
-    protected:
-        double re;
-        double im; //czesc urojona
-
-    public:
-        ~Complex(void);
-        Complex(double re=0,double im=0);
-        Complex operator +(Complex &);
-        friend std::ostream & operator<< (std::ostream &out, Complex &l);
-        double modul();
-
-    void wypisz(std::ostream &out)
-    {
-        out<<*this;
-    }
-};
-
 #endif /* LICZBY_H header gurad */
