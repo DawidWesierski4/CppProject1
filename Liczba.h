152b38876d0384842e5918b7ecdb43049b8f0764
DawidWesierski4 <dawidwesierski4@gmail.com>
2022-11-16 20:43:51 +0100

    add files basic structure of the project

---
+++ Liczba.h
@@ -0,0 +1,35 @@
+#ifndef LICZBY_H
+#define LICZBY_H
+
+#include <iostream>
+
+class Wartosc_Liczbowa
+{
+    public:
+    virtual double modul() = 0; //abs
+    virtual void wypisz(std::ostream &out) = 0;
+    virtual ~Wartosc_Liczbowa()
+    {}
+};
+
+class Liczba : public Wartosc_Liczbowa
+{
+    double re; //czesc rzeczywista 
+public:
+    Liczba operator+(Liczba & right)
+    {
+        return Liczba (this->re+right.re);
+    }
+
+    double modul();
+    Liczba(double re);
+    ~Liczba(void);
+    friend std::ostream & operator<< (std::ostream &out, Liczba &l);
+
+    void wypisz(std::ostream &out)
+    {
+        out<<*this;
+    }
+};
+
+#endif /* LICZBY_H header gurad */
---
+++ source1.cpp
@@ -0,0 +1,6 @@
+#include "Liczba.h"
+
+int main()
+{
+    std::cout<<"wtf this works"<<std::endl;
+}
